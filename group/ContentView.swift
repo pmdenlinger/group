12b9d1e6ecd2729b831362414a25157569befce5
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 10:39:11 -0700

    Group code added together.

--- group/ContentView.swift
+++ group/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 20){
+            Group{
+                
+//                Content to be grouped...
+                
+                VStack{
+                    Text("Hello")
+                    Text("World")
+                }
+                
+                HStack{
+                    Text("Welcome")
+                    Text("Guys")
+                }
+            }
+        }
     }
 }
 
