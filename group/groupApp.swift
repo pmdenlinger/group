6397c22f9fdebaeed4f3fb93ac50f18ad0c4951f
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 10:34:08 -0700

    Initial Commit

---
+++ group/groupApp.swift
@@ -0,0 +1,17 @@
+//
+//  groupApp.swift
+//  group
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct groupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
